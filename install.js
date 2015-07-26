ab3c42b01f6182eed0fbe27372c70018b37e8a02
k-kinzal <log@logn.in>
2015-07-26 23:24:23 +0900

    support retrying

--- install.js
+++ install.js
@@ -2,36 +2,43 @@
 
 var Promise = require('bluebird');
 
-Promise.resolve().then(function() {
-  return require('./src/fsc')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('fsc binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scala')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scala binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scalac')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scalac binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scaladoc')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scaladoc binary install successfully')
-           });
-}).then(function() {
-  return require('./src/scalap')
-           .runAsync(['-version'])
-           .then(function() {
-             console.log('scalap binary install successfully')
-           });
-}).catch(function(err) {
-  throw err;
-});
+(function install(retry) {
+  Promise.resolve().then(function() {
+    return require('./src/fsc')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('fsc binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scala')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scala binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scalac')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scalac binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scaladoc')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scaladoc binary install successfully')
+             });
+  }).then(function() {
+    return require('./src/scalap')
+             .runAsync(['-version'])
+             .then(function() {
+               console.log('scalap binary install successfully')
+             });
+  }).catch(function(err) {
+      if (retry < 3) {
+        console.warn('Retrying: ' + err);
+        setTimeout(install, 3000, retry+1);
+        return;
+      }
+      console.error(err);
+  });
+})(0);
